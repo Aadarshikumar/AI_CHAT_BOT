052468bf76b27d860c9087577dfe32fd8ca6dceb
Aadarsh kumar <aadarsh21@navgurukul.org>
2026-01-30 21:57:44 +0530

    basic home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Welcome to SmartAIBot</h1> */}
+      {/* Your app components go here */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
